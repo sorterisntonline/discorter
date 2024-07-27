54ad6fe33ec06e2c76e448b40d00e6f383b30f98
tommy-mor <thmorriss@gmail.com>
2024-07-26 20:56:11 -0700

    this is awesom
    fixed secrets

--- src/electric_starter_app/discord.clj
+++ src/electric_starter_app/discord.clj
@@ -1,10 +1,51 @@
 (ns electric-starter-app.discord
   (:require
    [clojure.edn :as edn]
-   [clojure.core.async :as a])
+   [clojure.core.async :as a]
+   [clojure.data.json :as json])
   (:import [org.eclipse.jetty.websocket.client WebSocketClient]
            [org.eclipse.jetty.websocket.api Session WebSocketAdapter]
            [java.net URI]))
+
+(def APP-ID "1266599133179940874")
+(def PUBLIC-KEY "e270e03e1923767654eee8c353d105fb96946d127c4a773f5cd5e00c8fbcd3d3")
+(def TOKEN (:token (read-string (slurp "secrets.edn"))))
+
+(def session (atom nil))
+(def last-sequence-number (atom nil))
+(def heartbeater (atom nil))
+(def resume-url (atom nil))
+
+(defn send-data [d]
+  (.. @session (getRemote) (sendString (json/write-str d))))
+
+(defn send-heartbeat []
+  (println "sending heartbeat")
+  (try (send-data {:op 1 :d @last-sequence-number})
+       (catch Exception e
+         (println "Error in heartbeater" e))))
+
+"
+t = which event
+d = data
+s = sequence number
+"
+
+(defn respond-hello [message]
+  (def message message)
+  (Thread/sleep 100)
+  (when-not @heartbeater
+    (reset! heartbeater (future (loop [] (Thread/sleep (-> message :d :heartbeat_interval))
+                                      (when (.isOpen @session)
+                                        (send-heartbeat)
+                                        (recur))))))
+  (Thread/sleep 100)
+
+  (send-data {:op 2 :d {:token TOKEN
+                        :intents 8
+                        :properties {:os "linux"
+                                     :browser "sorter"
+                                     :device "jvm"}}}))
 
 (defn create-socket []
   (proxy [WebSocketAdapter] []
@@ -12,7 +53,16 @@
       (println "WebSocket Connected"))
     
     (onWebSocketText [^String message]
-      (println "Received message:" message))
+      (let [message (json/read-str message {:key-fn keyword})]
+        (prn "Received message:" message)
+        (reset! last-sequence-number (:s message))
+        (def message
+          message)
+        (prn (case (:op message)
+               10 (respond-hello message)
+               11 :nothing
+               0 (reset! resume-url (:resume_gateway_url (:d message)))
+               :nothing))))
     
     (onWebSocketClose [statusCode reason]
       (println "WebSocket Closed. Status:" statusCode "Reason:" reason))
@@ -23,20 +73,20 @@
 (defn connect-websocket [url]
   (let [client (WebSocketClient.)
         socket (create-socket)]
-    (try
-      (.start client)
-      (let [future (.connect client socket (URI. url))
-            session (.get future)]
-        (def session session)
-        (.. session (getRemote) (sendString "rst")))
-      (catch Exception e
-        (println "Error:" (.getMessage e)))
-      (finally
-        ;; In a real application, you'd keep the client running and close it when done
-        ;; (.stop client)
-        ))))
+    
+    (.start client)
+    (let [future (.connect client socket (URI. url))
+          sesh (.get future)]
+      (reset! session sesh))))
+
+(defn close []
+  (.close @session)
+  (future-cancel @heartbeater))
 
 ;; Usage
 (comment
   (connect-websocket "wss://gateway.discord.gg")
+
+  (.close @session)
+  
   org.eclipse.jetty.websocket.common.WebSocketSession)
